2cc951f7e891a83fcac89026c601dc0276f9a3e1
ezindpa <pavan.indukuri@ericsson.com>
2022-01-12 22:06:02 +0100

    create token.pickle file in th same folder to avoid messing up other projects

--- book_badminton_courts.py
+++ book_badminton_courts.py
@@ -223,8 +223,9 @@ def get_calendar_service():
         )
 
     creds = None
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    token_file = os.path.join(current_dir, "token.pickle")
+    if os.path.exists(token_file):
+        with open(token_file, "rb") as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -232,7 +233,7 @@ def get_calendar_service():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(credentails_file, scopes)
             creds = flow.run_local_server(port=0)
-        with open("token.pickle", "wb") as token:
+        with open(token_file, "wb") as token:
             pickle.dump(creds, token)
     service = build("calendar", "v3", credentials=creds)
     return service
